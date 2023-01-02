a419793d38e20e41558b3102d15992bcf0ac32ad
george <jaber.george2@gmail.com>
2023-01-02 12:39:25 +0200

    create the tokens

--- compiler design/src/Lexical_Analyzer.java
+++ compiler design/src/Lexical_Analyzer.java
@@ -1,20 +1,56 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
 
 public class Lexical_Analyzer {
     FileReader file_reader = new FileReader("code.txt");
     BufferedReader br = new BufferedReader(file_reader);
 
+    LinkedList<Token> Token_list = new LinkedList<>();
+
+
     public Lexical_Analyzer() throws IOException {
 
        char c = ((char) br.read());
 
-       switch (c)
+       while(c!=-1)
        {
 
+           if(isOperator(c))
+           {
+              Token_list.add(new Token(Token_Category.Operator,c+""));
+           }
+           else if(Character.isDigit(c))
+           {
+
+              Token_list.add(new Token(Token_Category.Constant,c+""));
+           }
+           else if(c==';')
+           {
+
+              Token_list.add(new Token(Token_Category.Keyword,c+""));
+           }
+           else
+           {
+               Token_list.add(new Token(Token_Category.Identifier,c+""));
+
+           }
        }
 
+    }
+
+    public boolean isOperator(char c)
+    {
+        char[] Operators = {'+', '-' , '*' , '/' , '='};
+        for (int i=0 ;i<Operators.length;i++)
+        {
+            if(c==Operators[i])
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
